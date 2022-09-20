7846f29de0b6121782b14996ef20a2a3f5a7f742
Mike2611 <mpuentes2611@gmail.com>
2022-09-20 12:45:31 -0500

    Set up mongoose connection

--- locallibrary/app.js
+++ locallibrary/app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// set up mongoose connection
+// Set up mongoose connection
+const mongoose = require("mongoose");
+const mongoDB = "mongodb+srv://mike2611:mike2611password@cluster0.lxpxryr.mongodb.net/local_library?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
